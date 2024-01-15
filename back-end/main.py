a7e825feabe1c8a5788da6b8711875c00dc8e549
kaya-atakan <93atakankaya@gmail.com>
2024-01-16 00:46:32 +0100

    enabling  Cross-Origin Resource Sharing (CORS)

--- back-end/main.py
+++ back-end/main.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
 import requests
 import pandas as pd
 from io import StringIO
 import numpy as np
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# Create an instance of the FastAPI application
 app = FastAPI()
+
+# Add CORS middleware to the application instance
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 async def root():
