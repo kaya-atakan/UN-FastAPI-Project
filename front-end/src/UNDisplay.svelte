206f900c7205eae10585eee356d0fa28f02fb943
kaya-atakan <93atakankaya@gmail.com>
2024-01-16 01:13:17 +0100

    updating folder structure

--- front-end/src/UNDisplay.svelte
+++
@@ -1,6 +0,0 @@
-<!-- UNDisplay.svelte-->
-<script>
-    import UNStatistics from './UNStatistics.svelte';
-</script>
-
-<UNStatistics />
---
+++ front-end/src/routes/UNDisplay.svelte
@@ -0,0 +1,6 @@
+<!-- UNDisplay.svelte-->
+<script>
+    import UNStatistics from '../components/UNStatistics.svelte';
+</script>
+
+<UNStatistics />
